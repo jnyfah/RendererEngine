317c8e8f893f6d6481c982caa7fe10d4527b655c
Jean Philippe <jeanphilippe02666592@outlook.fr>
2024-11-23 15:30:39 +0100

    Panzerfaust - Adding project removal implementation (#389)
    * added project removal impl
    
    * completed delete implementation

--- Panzerfaust/ViewModels/ProjectViewModel.cs
+++ Panzerfaust/ViewModels/ProjectViewModel.cs
@@ -5,6 +5,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reactive;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,17 +14,38 @@ namespace Panzerfaust.ViewModels
     internal class ProjectViewModel : ViewModelBase
     {
         private readonly Project _project;
+        private Interaction<MessageBoxWindowViewModel, bool>? _deleteProjectInteraction;
 
         public string Name => _project.Name;
         public string Path => _project.Fullpath;
         public string UpdatedDate => _project.UpdateDate.ToShortDateString();
 
         public ReactiveCommand<Unit, Unit> OpenProjectCommand { get; }
+        public ReactiveCommand<Unit, Unit> DeleteProjectCommand { get; }
 
-        public ProjectViewModel(Project p)
+        public ProjectViewModel(Project p, Interaction<MessageBoxWindowViewModel, bool>? interaction = null)
         {
             _project = p;
+            _deleteProjectInteraction = interaction;
             OpenProjectCommand = ReactiveCommand.Create(OnOpenProjectCommand);
+            DeleteProjectCommand = ReactiveCommand.CreateFromTask(OnDeleteProjectCommand);
+        }
+
+        public void SetRemovalInteraction(Interaction<MessageBoxWindowViewModel, bool> interaction) => _deleteProjectInteraction = interaction;
+
+        private async Task OnDeleteProjectCommand()
+        {
+            if (_deleteProjectInteraction == null) { return; }
+
+            var result = await _deleteProjectInteraction.Handle(new MessageBoxWindowViewModel());
+            if (result)
+            {
+                var projectService = App.Current?.ServiceProvider?.GetService<Service.IProjectService>();
+                if (projectService == null) { return; }
+
+                await projectService.DeleteAsync(_project);
+                MessageBus.Current.SendMessage<(string, ProjectViewModel)>((Message.DeleteAction, this));
+            }
         }
 
         private async void OnOpenProjectCommand()
--- Panzerfaust/Views/MainWindow.axaml.cs
+++ Panzerfaust/Views/MainWindow.axaml.cs
@@ -2,15 +2,21 @@ using Avalonia.Controls;
 using Avalonia.ReactiveUI;
 using Panzerfaust.ViewModels;
 using ReactiveUI;
+using System.Reactive;
 
 namespace Panzerfaust.Views
 {
     internal partial class MainWindow : ReactiveWindow<MainWindowViewModel>
     {
         public MainWindow()
-        {        
+        {
             InitializeComponent();
-            this.WhenActivated(action => action(ViewModel!.NewProjectDialog.RegisterHandler(DialogHandler)));
+            this.WhenActivated(
+                action =>
+                {
+                    ViewModel!.NewProjectDialog.RegisterHandler(DialogHandler);
+                    ViewModel!.DeleteProjectInteraction.RegisterHandler(DeleteProjectInteractionHandler);
+                });
         }
 
         private async Task DialogHandler(IInteractionContext<ProjectWindowViewModel, ProjectViewModel?> interactionContext)
@@ -22,6 +28,13 @@ namespace Panzerfaust.Views
 
             var result = await dialog.ShowDialog<ProjectViewModel?>(this);
             interactionContext.SetOutput(result);
+        }
+
+        private async Task DeleteProjectInteractionHandler(IInteractionContext<MessageBoxWindowViewModel, bool> context)
+        {
+            var dialog = new MessageBoxWindow { DataContext = context.Input};
+            var result = await dialog.ShowDialog<bool>(this);
+            context.SetOutput(result);
         }
     }
 }
